410e7f6457954bdc68114d572c75d9bb7f23f40a
unknown <christy.g.lee@gmail.com>
2020-10-17 22:15:31 -0700

    updated tables

--- employeeSeeds.sql
+++ employeeSeeds.sql
@@ -4,26 +4,30 @@ CREATE DATABASE employee_tracker_DB;
 
 USE employee_tracker_DB;
 
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE employees (
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
-  manager_id INT,
+  manager_id INT NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30),
-  salary DECIMAL(10,2) NULL,
-  department_id INT,
-  PRIMARY KEY (id)
-);
 
-CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30),
-);
+
+
 
 
