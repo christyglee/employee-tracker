6fc080aacc44b1459a01364fec58e899d3381a84
unknown <christy.g.lee@gmail.com>
2020-10-17 19:11:53 -0700

    established dependencies and local host connection

---
+++ employeeCRUD.js
@@ -0,0 +1,25 @@
+//Dependencies
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// Connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "password",
+  database: "employee_trackerDB"
+});
+
+// Connect to the mysql server and sql database
+connection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+  });
