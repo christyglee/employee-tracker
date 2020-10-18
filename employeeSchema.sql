010278afc166b6b6fc054b083d7242691f6f78ae
unknown <christy.g.lee@gmail.com>
2020-10-17 23:03:58 -0700

    renamed file

---
+++ employeeSchema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS employee_Tracker_DB;
+
+CREATE DATABASE employee_Tracker_DB;
+
+USE employeeTracker_DB;
+
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NULL, 
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL (10,2) NULL,
+  department_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+
+
+
+
+
