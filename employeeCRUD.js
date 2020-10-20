e03778ac945b8fc1a6dc6cee9362ea100c7c2594
unknown <christy.g.lee@gmail.com>
2020-10-19 21:45:45 -0700

    start function prompt

--- employeeCRUD.js
+++ employeeCRUD.js
@@ -1,6 +1,7 @@
 //Dependencies
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var consoleTable = require("console.table");
 
 // Connection information for the sql database
 var connection = mysql.createConnection({
@@ -23,3 +24,22 @@ connection.connect(function(err) {
     // run the start function after the connection is made to prompt the user
     start();
   });
+
+  function start() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "rawlist",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "View All Employees By Department",
+          "View All Employees By Manager",
+          "Add Employee",
+          "Remove Employee",
+          "Update Employee Role",
+          "Update Employee Manager"
+        ]
+      })
+
+    }
