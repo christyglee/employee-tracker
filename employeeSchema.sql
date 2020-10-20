835b5fabc835913706a886a20b2a3bb99a74c921
unknown <christy.g.lee@gmail.com>
2020-10-19 23:06:42 -0700

    updated typo error

--- employeeSchema.sql
+++ employeeSchema.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS employee_Tracker_DB;
 
 CREATE DATABASE employee_Tracker_DB;
 
-USE employeeTracker_DB;
+USE employee_Tracker_DB;
 
 CREATE TABLE department(
   id INT NOT NULL AUTO_INCREMENT,
